a8a9ea7d4b5a4c0bba82bed63e83a9846a62a18f
riccio-ryu <40678359+RiccioRyu@users.noreply.github.com>
2021-12-10 15:43:49 +0900

    aa

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import App from './App';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
+
+
+const queryClient = new QueryClient()
+
+ReactDOM.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
